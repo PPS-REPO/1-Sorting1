64b5fd664e213a614ce0adde86c590bc6fccc2d5
pps-admin <pps.github@gmail.com>
2023-04-19 20:00:26 +0900

    Update: path 'generator/generator_manual.py' - Updated by 'rn'

--- generator/generator_manual.py
+++ generator/generator_manual.py
@@ -8,3 +8,16 @@ elif t == 2:
     print('4\n-1\n-2\n-1\n1')
 elif t == 3:
     print('8\n8\n7\n6\n5\n1\n2\n3\n4')
+elif t == 4:
+    print(1)
+    print(-1000000)
+elif t == 5:
+    n = 200000
+    print(n)
+    for i in range(n):
+        print((i - 100000) * 10)
+elif t == 6:
+    n = 199999
+    print(n)
+    for i in range(n):
+        print((i - 99998) * 10)
