245fb414f32a694c61b073abd450a10f4e74aad3
pps-admin <pps.github@gmail.com>
2023-04-19 19:52:15 +0900

    Update: path 'solution/solution.cc' - Updated by 'rn'

--- solution/solution.cc
+++ solution/solution.cc
@@ -1,8 +1,13 @@
+#include <algorithm>
 #include <cstdio>
 
+int ar[202020];
+
 int main() {
-    int a, b;
-    scanf("%d%d", &a, &b);
-    printf("%d", a + b);
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; ++i) scanf("%d", ar + i);
+    std::sort(ar, ar + n);
+    for (int i = 0; i < n; ++i) printf("%d\n", ar[i]);
     return 0;
 }
